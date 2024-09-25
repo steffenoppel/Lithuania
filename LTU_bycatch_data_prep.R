d252dd6a1eab78a03f8ee66b61096155c3655434
Steffen Oppel <steffen.oppel@gmail.com>
2024-09-25 10:02:42 +0200

    updated data prep to include sun and dawn info
    added sunrise and sunset and natical dawn and dusk times to differentiate between trips that were in darkness and those that were not

--- LTU_bycatch_data_prep.R
+++ LTU_bycatch_data_prep.R
@@ -13,6 +13,8 @@
 
 ## updated on 30 August 2024 after receiving final data from Rasa Morkune
 
+## updated on 25 Sept 2025 to include columns of nighttime (with cutoffs of sunset, civil, and nautical twilight)
+
 
 ### Load libraries
 library(ggplot2)
@@ -21,6 +23,7 @@ library(tidyverse)
 library(stringr)
 library(readxl)
 library(lubridate)
+library(suncalc) ## to estimate daytime
 filter<-dplyr::filter
 select<-dplyr::select
 #library(marmap)
@@ -42,6 +45,7 @@ tripcols <- as.character(read_excel("data/2019-2024 all trials final table.xlsx"
 trips <- read_excel("data/2019-2024 all trials final table.xlsx", sheet="tbl_trip", skip=3, col_names = tripcols)
 head(trips)
 setcols <- as.character(read_excel("data/2019-2024 all trials final table.xlsx", sheet="tbl_set", n_max = 1, col_names = FALSE))
+setcols[c(4:5)]<-c("TrialType3hSS","TrialType2hSS")
 sets <- read_excel("data/2019-2024 all trials final table.xlsx", sheet="tbl_set", skip=3, col_names = setcols[1:33])
 
 ## combine date and time into a single field
@@ -53,6 +57,21 @@ head(sets)
 gearcols <- as.character(read_excel("data/2019-2024 all trials final table.xlsx", sheet="tbl_gear", n_max = 1, col_names = FALSE))
 gear <- read_excel("data/2019-2024 all trials final table.xlsx", sheet="tbl_gear", skip=3, col_names = gearcols[1:9])
 head(gear)
+
+## calculate sunset and sunrise times for setting and hauling and calculate difference between sunset and deployment (and sunrise and haul)
+sets$sunset<-suncalc::getSunlightTimes(date=as.Date(sets$Depl_Date),lat=55.737232,lon=20.988563,keep="sunset")$sunset
+sets$sunrise<-suncalc::getSunlightTimes(date=as.Date(sets$Haul_Date),lat=55.737232,lon=20.988563,keep="sunrise")$sunrise
+sets$nauticalDusk<-suncalc::getSunlightTimes(date=as.Date(sets$Depl_Date),lat=55.737232,lon=20.988563,keep="nauticalDusk")$nauticalDusk
+sets$nauticalDawn<-suncalc::getSunlightTimes(date=as.Date(sets$Haul_Date),lat=55.737232,lon=20.988563,keep="nauticalDawn")$nauticalDawn
+
+sets <- sets %>%
+  mutate(deplSSdiff=as.numeric(difftime(sunset, Depl_Date, unit="hours")), haulSRdiff=as.numeric(difftime(Haul_Date, sunrise, unit="hours")),
+         deplNDdiff=as.numeric(difftime(nauticalDusk, Depl_Date, unit="hours")), haulNDdiff=as.numeric(difftime(Haul_Date, nauticalDawn, unit="hours"))) %>%
+  rowwise() %>%
+  mutate(daylightTripoverlap=max(deplSSdiff,haulSRdiff, na.rm=T), twilightTripoverlap=max(deplNDdiff,haulNDdiff, na.rm=T)) %>%
+  mutate(SStrip=ifelse(daylightTripoverlap<0,"Night",Trial_type_by_fishermen)) %>%  ## this could be adjusted to use 1 or 2 hr tolerance to allow fishing within 1-2 hrs of sunset
+  mutate(TLtrip=ifelse(twilightTripoverlap<0,"Night",Trial_type_by_fishermen))   ## this could be adjusted to use 1 or 2 hr tolerance to allow fishing within 1-2 hrs of twilight
+sets %>% select(Trial_type_by_fishermen,TrialType2hSS,daylightTripoverlap,SStrip,deplSSdiff,haulSRdiff,Depl_Date,sunset,Haul_Date,sunrise)
 
 # Read the data from sheets in Excel file - stripping Lithuanian header in first row
 fishcols <- as.character(read_excel("data/2019-2024 all trials final table.xlsx", sheet="tbl_fish", n_max = 1, col_names = FALSE))
@@ -74,7 +93,7 @@ head(bycatch)
 ### calculate fishing effort per set
 effortsummary<-sets  %>%
   left_join(gear, by="Set_ID") %>%
-  select(Trip_ID,Set_ID,Trial_type_by_fishermen,Season, Month,Depl_Date,Haul_Date, Total_net_area,Fishing_depth) %>%
+  select(Trip_ID,Set_ID,Trial_type_by_fishermen,Season, Month,Depl_Date,Haul_Date, Total_net_area,Fishing_depth,SStrip,TLtrip,TrialType3hSS,TrialType2hSS,daylightTripoverlap,twilightTripoverlap) %>%
   mutate(Hours_deployed=as.numeric(difftime(Haul_Date,Depl_Date, units="hours"))) %>%  ## this does not work because it cannot deal with numbers >23:59
   mutate(Hours_deployed=ifelse(is.na(Hours_deployed),12,Hours_deployed)) %>%    ## make up number for sets with no soak time
   mutate(Hours_deployed=ifelse(Hours_deployed==0,12,Hours_deployed)) %>%   ## make up number for sets with 0 soak time
@@ -137,7 +156,7 @@ head(alldata)
 
 #### SAVE DATA FOR ANALYSIS
 analysisdata<-alldata %>%
-  select(Trip_ID,Set_ID,Trial_type_by_fishermen,Season,Month,Total_net_area, Hours_deployed, Fishing_depth,Effort, catch,bycatch, CPUE, BPUE,LTBPUE,SDBPUE) %>%
+  select(Trip_ID,Set_ID,Trial_type_by_fishermen,Season,Month,Total_net_area, Hours_deployed, Fishing_depth,Effort, catch,bycatch, CPUE, BPUE,LTBPUE,SDBPUE,,SStrip,TLtrip,TrialType3hSS,TrialType2hSS,daylightTripoverlap,twilightTripoverlap) %>%
   left_join(sets[,c(2,4,30)], by="Set_ID")
 saveRDS(analysisdata,"data/LIT_bycatch_data_formatted.rds")
 
@@ -148,14 +167,39 @@ saveRDS(analysisdata,"data/LIT_bycatch_data_formatted.rds")
 #####
 #####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~########
 
-SUMMARY <- alldata %>%
+SUMMARY1 <- alldata %>%
   mutate(Trial_type_by_fishermen=ifelse(Trial_type_by_fishermen=="Control2","Control",Trial_type_by_fishermen)) %>%
   group_by(Season, Trial_type_by_fishermen) %>%
-  summarise(n_sets=length(unique(Set_ID)), Tot_effort=sum(Effort,na.rm=T), Tot_fish=sum(catch,na.rm=T), Tot_bycatch=sum(bycatch,na.rm=T), CPUE=mean(CPUE,na.rm=T), BPUE=mean(BPUE,na.rm=T))
+  summarise(n_sets=length(unique(Set_ID)), Tot_effort=sum(Effort,na.rm=T), Tot_fish=sum(catch,na.rm=T), Tot_bycatch=sum(bycatch,na.rm=T), CPUE=mean(CPUE,na.rm=T), BPUE=mean(BPUE,na.rm=T)) %>%
+  mutate(NightDefinition="by fisherman")
 
-SUMMARY
+SUMMARY2 <- alldata %>%
+  mutate(Trial_type_by_fishermen=if_else(TrialType3hSS=="Control2","Control",TrialType3hSS)) %>%
+  group_by(Season, Trial_type_by_fishermen) %>%
+  summarise(n_sets=length(unique(Set_ID)), Tot_effort=sum(Effort,na.rm=T), Tot_fish=sum(catch,na.rm=T), Tot_bycatch=sum(bycatch,na.rm=T), CPUE=mean(CPUE,na.rm=T), BPUE=mean(BPUE,na.rm=T)) %>%
+  mutate(NightDefinition="within 3 hrs of sunset")
 
-mean(alldata$Effort)
+SUMMARY3 <- alldata %>%
+  mutate(Trial_type_by_fishermen=if_else(TrialType2hSS=="Control2","Control",TrialType2hSS)) %>%
+  group_by(Season, Trial_type_by_fishermen) %>%
+  summarise(n_sets=length(unique(Set_ID)), Tot_effort=sum(Effort,na.rm=T), Tot_fish=sum(catch,na.rm=T), Tot_bycatch=sum(bycatch,na.rm=T), CPUE=mean(CPUE,na.rm=T), BPUE=mean(BPUE,na.rm=T)) %>%
+  mutate(NightDefinition="within 2 hrs of sunset")
+
+SUMMARY4 <- alldata %>%
+  mutate(Trial_type_by_fishermen=if_else(SStrip=="Control2","Control",SStrip)) %>%
+  group_by(Season, Trial_type_by_fishermen) %>%
+  summarise(n_sets=length(unique(Set_ID)), Tot_effort=sum(Effort,na.rm=T), Tot_fish=sum(catch,na.rm=T), Tot_bycatch=sum(bycatch,na.rm=T), CPUE=mean(CPUE,na.rm=T), BPUE=mean(BPUE,na.rm=T)) %>%
+  mutate(NightDefinition="sunset to sunrise")
+
+SUMMARY5 <- alldata %>%
+  mutate(Trial_type_by_fishermen=if_else(TLtrip=="Control2","Control",TLtrip)) %>%
+  group_by(Season, Trial_type_by_fishermen) %>%
+  summarise(n_sets=length(unique(Set_ID)), Tot_effort=sum(Effort,na.rm=T), Tot_fish=sum(catch,na.rm=T), Tot_bycatch=sum(bycatch,na.rm=T), CPUE=mean(CPUE,na.rm=T), BPUE=mean(BPUE,na.rm=T)) %>%
+  mutate(NightDefinition="nautical dusk to dawn")
+
+SUMMARY<-bind_rows(SUMMARY1,SUMMARY2,SUMMARY3,SUMMARY4,SUMMARY5)
+
+fwrite(SUMMARY,"output/Raw_data_summary_by_night_type.csv")
 
 
 
@@ -166,6 +210,7 @@ mean(alldata$Effort)
 #####
 #####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~########
 
+## for fisherman NIGHT definition
 alldata %>%
   mutate(Trial_type_by_fishermen=if_else(Trial_type_by_fishermen=="Control2","Control",Trial_type_by_fishermen)) %>%
   
@@ -189,6 +234,121 @@ alldata %>%
         panel.border = element_blank())
 
 ggsave("output/LTU_raw_bycatch_data.jpg", width=11, height=9)
+
+
+
+## for NIGHT definition with 3 hrs around sunset
+
+alldata %>%
+  mutate(Trial_type_by_fishermen=if_else(TrialType3hSS=="Control2","Control",TrialType3hSS)) %>%
+  
+  ggplot(aes(y=BPUE*12322.71, x=Trial_type_by_fishermen)) +
+  #geom_point(size=2, colour="firebrick",position=position_jitter(width = 0.15, height = 0))+
+  geom_violin(colour="firebrick")+
+  #scale_y_continuous(limits=c(-1.5,1.5), breaks=seq(-1.5,1.5,0.5)) +
+  geom_hline(aes(yintercept=0), colour="darkgrey", linetype=2) +
+  xlab("") +
+  ylab("N seabirds caught per average set effort") +
+  theme(panel.background=element_rect(fill="white", colour="black"), 
+        axis.text=element_text(size=16, color="black"), 
+        axis.title=element_text(size=18), 
+        strip.text=element_text(size=18, color="black"),
+        legend.text=element_text(size=14, color="black"),
+        legend.title=element_text(size=18, color="black"),
+        legend.key=element_blank(),
+        strip.background=element_rect(fill="white", colour="black"), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        panel.border = element_blank())
+
+ggsave("output/LTU_raw_bycatch_data_3hrSS.jpg", width=11, height=9)
+
+
+
+## for NIGHT definition with 2 hrs around sunset
+
+alldata %>%
+  mutate(Trial_type_by_fishermen=if_else(TrialType2hSS=="Control2","Control",TrialType2hSS)) %>%
+  
+  ggplot(aes(y=BPUE*12322.71, x=Trial_type_by_fishermen)) +
+  #geom_point(size=2, colour="firebrick",position=position_jitter(width = 0.15, height = 0))+
+  geom_violin(colour="firebrick")+
+  #scale_y_continuous(limits=c(-1.5,1.5), breaks=seq(-1.5,1.5,0.5)) +
+  geom_hline(aes(yintercept=0), colour="darkgrey", linetype=2) +
+  xlab("") +
+  ylab("N seabirds caught per average set effort") +
+  theme(panel.background=element_rect(fill="white", colour="black"), 
+        axis.text=element_text(size=16, color="black"), 
+        axis.title=element_text(size=18), 
+        strip.text=element_text(size=18, color="black"),
+        legend.text=element_text(size=14, color="black"),
+        legend.title=element_text(size=18, color="black"),
+        legend.key=element_blank(),
+        strip.background=element_rect(fill="white", colour="black"), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        panel.border = element_blank())
+
+ggsave("output/LTU_raw_bycatch_data_2hrSS.jpg", width=11, height=9)
+
+
+
+## for NIGHT definition with sunset
+
+alldata %>%
+  mutate(Trial_type_by_fishermen=if_else(SStrip=="Control2","Control",SStrip)) %>%
+  
+  ggplot(aes(y=BPUE*12322.71, x=Trial_type_by_fishermen)) +
+  #geom_point(size=2, colour="firebrick",position=position_jitter(width = 0.15, height = 0))+
+  geom_violin(colour="firebrick")+
+  #scale_y_continuous(limits=c(-1.5,1.5), breaks=seq(-1.5,1.5,0.5)) +
+  geom_hline(aes(yintercept=0), colour="darkgrey", linetype=2) +
+  xlab("") +
+  ylab("N seabirds caught per average set effort") +
+  theme(panel.background=element_rect(fill="white", colour="black"), 
+        axis.text=element_text(size=16, color="black"), 
+        axis.title=element_text(size=18), 
+        strip.text=element_text(size=18, color="black"),
+        legend.text=element_text(size=14, color="black"),
+        legend.title=element_text(size=18, color="black"),
+        legend.key=element_blank(),
+        strip.background=element_rect(fill="white", colour="black"), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        panel.border = element_blank())
+
+ggsave("output/LTU_raw_bycatch_data_SS.jpg", width=11, height=9)
+
+
+
+
+## for NIGHT definition with nautical twilight
+
+alldata %>%
+  mutate(Trial_type_by_fishermen=if_else(TLtrip=="Control2","Control",TLtrip)) %>%
+  
+  ggplot(aes(y=BPUE*12322.71, x=Trial_type_by_fishermen)) +
+  #geom_point(size=2, colour="firebrick",position=position_jitter(width = 0.15, height = 0))+
+  geom_violin(colour="firebrick")+
+  #scale_y_continuous(limits=c(-1.5,1.5), breaks=seq(-1.5,1.5,0.5)) +
+  geom_hline(aes(yintercept=0), colour="darkgrey", linetype=2) +
+  xlab("") +
+  ylab("N seabirds caught per average set effort") +
+  theme(panel.background=element_rect(fill="white", colour="black"), 
+        axis.text=element_text(size=16, color="black"), 
+        axis.title=element_text(size=18), 
+        strip.text=element_text(size=18, color="black"),
+        legend.text=element_text(size=14, color="black"),
+        legend.title=element_text(size=18, color="black"),
+        legend.key=element_blank(),
+        strip.background=element_rect(fill="white", colour="black"), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        panel.border = element_blank())
+
+ggsave("output/LTU_raw_bycatch_data_TL.jpg", width=11, height=9)
+
+
 
 
 
