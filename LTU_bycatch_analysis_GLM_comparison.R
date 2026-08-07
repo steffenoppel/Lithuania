b763923bc8996f039b12e7b2bf690a698fb45fa2
Steffen Oppel <steffen.oppel@gmail.com>
2026-08-07 18:20:28 +0200

    finalised comparison of modelling methods
    compared GLMs and bootstrap approaches, which all yield the same output

--- LTU_bycatch_analysis_GLM_comparison.R
+++ LTU_bycatch_analysis_GLM_comparison.R
@@ -4,20 +4,8 @@
 ### ##################################################
 
 ## goal is to examine whether kites and night fishing reduce seabird bycatch
-## also test whether fish catch is maintained
-## major complication is that some sets declared as 'night' fishing straddle daytime
-## these are specified in Adj_trial_types and Adj_trial subtypes
-
-## 9 Feb 2024: added a compositional analysis to test whether bycatch depth distribution and effort are significantly different
-## 9 Feb 2024: added species-specific analyses
-## re-run on 30 August 2024 after receiving final data from Rasa Morkune
-## updated on 25 Sept to use updated definition of 'night' (many fisherman were very liberal with that term)
-## revised analysis based on Julius Morkunas email suggestion (7 Oct 2024): create matrix of sunset and sunrise time diffs
-
-### UPDATED 9 AUG 2026: reviewer demanded BCAboot and NB GLM
-
 ## added tests: will GLM meet distribution assumptions?
-## does boot distribution approach stable value?
+## REVISION IN AUGUST 2026
 
 ### Load libraries
 library(ggplot2)
@@ -31,18 +19,13 @@ library(DHARMa)
 library(performance)
 library(AER)
 library(marginaleffects)
+library(emmeans)
 filter<-dplyr::filter
 select<-dplyr::select
 
 
 
-
-
-#####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~########
-#####
-#####     DATA IMPORT AND MANIPULATION -------------------------
-#####
-#####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~########
+# 1.    DATA IMPORT AND MANIPULATION -------------------------------
 
 try(setwd("C:\\STEFFEN\\RSPB\\Marine\\Bycatch\\GillnetBycatch\\Analysis\\Lithuania"), silent=T)
 try(setwd("C:\\STEFFEN\\OneDrive - THE ROYAL SOCIETY FOR THE PROTECTION OF BIRDS\\STEFFEN\\RSPB\\Marine\\Bycatch\\GillnetBycatch\\Analysis\\Lithuania"), silent=T)
@@ -165,10 +148,10 @@ OUT
 
 
 # 5.   ZERO-INFLATED POISSON -------------------------------
-
+## cannot estimate SE
 m_zip <- zeroinfl(
-  bycatch ~ Effort + night * kite |
-    Effort + night + kite,
+  bycatch ~ Effort + night* kite |
+    Effort + night* kite,
   dist = "poisson",
   data = data
 )
@@ -181,8 +164,8 @@ summary(m_zip)
 # 6.   ZERO-INFLATED NEGATIVE BINOMIAL -------------------------------
 
 m_zinb <- zeroinfl(
-  bycatch ~ Effort + night * kite |
-    Effort + night + kite,
+  bycatch ~ Effort + night |
+    Effort + kite,
   dist = "negbin",
   data = data
 )
@@ -191,11 +174,11 @@ summary(m_zinb)
 
 
 ## 6.1. assess model fit -------
-res_zinb <- simulateResiduals(m_zinb)
-plot(res_zinb)
-
-testDispersion(res_zinb)
-testZeroInflation(res_zinb)
+# res_zinb <- simulateResiduals(m_zinb)
+# plot(res_zinb)
+# 
+# testDispersion(res_zinb)
+# testZeroInflation(res_zinb)
 
 
 
@@ -219,7 +202,7 @@ AIC(
   m_zip,
   m_zinb,
   m_hurdle
-)
+) %>% arrange(AIC)
 
 
 
@@ -232,12 +215,12 @@ m_nb1 <- MASS::glm.nb(bycatch ~ Effort + night + kite,  data = data)
 AIC(m_nb1, m_nb3) 
 summary(m_nb1)
 
-## 4.1. assess model fit -------
-1 - pchisq(summary(m_nb)$deviance,
-           summary(m_nb)$df.residual
+## 9.1. assess model fit -------
+1 - pchisq(summary(m_nb1)$deviance,
+           summary(m_nb1)$df.residual
 )  ## GOF test suggests no fit at all!!
 
-res_nb <- simulateResiduals(m_nb)
+res_nb <- simulateResiduals(m_nb1)
 plot(res_nb)
 
 testDispersion(res_nb)
@@ -246,14 +229,8 @@ testZeroInflation(res_nb)
 
 
 
-## 4.2. predict response -------
-OUT<-cbind(plotdat, 
-           Mean = predict(m_nb, newdata = plotdat, type = "response"), 
-           SE = predict(m_nb, newdata = plotdat, type="response", se.fit = T)$se.fit
-) %>%
-  mutate(MODEL="NegBin", lcl=Mean-1.96*SE, ucl=Mean+19.6*SE) %>%
-  bind_rows(OUT)
-OUT
+## 9.2. predict response -------
+
 
 data$pred <- predict(
   m_nb1,
@@ -267,7 +244,7 @@ plot(data$pred,
 abline(0,1,col="red")
 
 
-library(emmeans)
+
 
 emmeans(
   m_nb1,
@@ -277,47 +254,236 @@ emmeans(
 
 
 
+
+
+## 9.3. predict response with parametric bootstrap -----------------------------
+# from https://stat.ethz.ch/pipermail/r-help/2008-December/182806.html
+
+
+
+predict.zeroinfl <- function(object, newdata, type = c("response", "prob"),
+                             
+                             se=FALSE,MC=1000,level=.95,
+                             
+                             na.action = na.pass, ...)
+  
+{
+  
+  type <- match.arg(type)
+  
+  
+  
+  ## if no new data supplied
+  
+  if(missing(newdata)){
+    
+    rval <- object$fitted.values
+    
+    if(!is.null(object$x)) {
+      
+      X <- object$x$count
+      
+      Z <- object$x$zero
+      
+    }
+    
+    else if(!is.null(object$model)) {
+      
+      X <- model.matrix(object$terms$count, object$model, contrasts = object$contrasts$count)
+      
+      Z <- model.matrix(object$terms$zero,  object$model, contrasts = object$contrasts$zero)	
+      
+    }
+    
+    else {
+      
+      stop("no X and/or Z matrices can be extracted from fitted model")
+      
+    }
+    
+    if(type == "prob") {
+      
+      mu <- exp(X %*% object$coefficients$count)[,1]
+      
+      phi <- object$linkinv(Z %*% object$coefficients$zero)[,1]
+      
+    }
+    
+  }
+  
+  else {
+    
+    mf <- model.frame(delete.response(object$terms$full), newdata, na.action = na.action, xlev = object$levels)
+    
+    X <- model.matrix(delete.response(object$terms$count), mf, contrasts = object$contrasts$count)
+    
+    Z <- model.matrix(delete.response(object$terms$zero),  mf, contrasts = object$contrasts$zero)
+    
+    
+    
+    mu <- exp(X %*% object$coefficients$count)[,1]
+    
+    phi <- object$linkinv(Z %*% object$coefficients$zero)[,1]
+    
+    rval <- (1-phi) * mu
+    
+  }   
+  
+  
+  
+  if(se & !is.null(X) & !is.null(Z)){
+    
+    require(mvtnorm)
+    
+    vc <- -solve(object$optim$hessian)
+    
+    kx <- length(object$coefficients$count)
+    
+    kz <- length(object$coefficients$zero)
+    
+    parms <- object$optim$par
+    
+    if(type!="prob"){
+      
+      yhat.sim <- matrix(NA,MC,dim(X)[1])
+      
+      for(i in 1:MC){
+        
+        cat(paste("MC iterate",i,"of",MC,"\n"))
+        
+        parms.sim <- rmvnorm(n=1,mean=parms,sigma=vc)
+        
+        beta <- parms.sim[1:kx]
+        
+        gamma <- parms.sim[(kx+1):(kx+kz)]
+        
+        mu.sim <- exp(X%*%beta)[,1]
+        
+        phi.sim <- object$linkinv(Z%*%gamma)[,1]
+        
+        yhat.sim[i,] <- (1-phi.sim)*mu.sim
+        
+      }
+      
+    }
+    
+    out <- list()
+    
+    out$lower <- apply(yhat.sim,2,quantile,(1-level)/2)
+    
+    out$upper <- apply(yhat.sim,2,quantile,1-((1-level)/2))
+    
+    out$se <- apply(yhat.sim,2,sd)
+    
+  }
+  
+  
+  
+  ## predicted probabilities
+  
+  if(type == "prob") {
+    
+    if(!is.null(object$y)) y <- object$y
+    
+    else if(!is.null(object$model)) y <- model.response(object$model)
+    
+    else stop("predicted probabilities cannot be computed for fits with y = FALSE and model = FALSE")
+    
+    
+    
+    yUnique <- min(y):max(y)
+    
+    nUnique <- length(yUnique)
+    
+    rval <- matrix(NA, nrow = length(rval), ncol = nUnique)
+    
+    dimnames(rval) <- list(rownames(X), yUnique)
+    
+    
+    
+    switch(object$dist,
+           
+           "poisson" = {
+             
+             rval[, 1] <- phi + (1-phi) * exp(-mu)
+             
+             for(i in 2:nUnique) rval[,i] <- (1-phi) * dpois(yUnique[i], lambda = mu)
+             
+           },
+           
+           "negbin" = {
+             
+             theta <- object$theta
+             
+             rval[, 1] <- phi + (1-phi) * dnbinom(0, mu = mu, size = theta)
+             
+             for(i in 2:nUnique) rval[,i] <- (1-phi) * dnbinom(yUnique[i], mu = mu, size = theta)
+             
+           },
+           
+           "geometric" = {
+             
+             rval[, 1] <- phi + (1-phi) * dnbinom(0, mu = mu, size = 1)
+             
+             for(i in 2:nUnique) rval[,i] <- (1-phi) * dnbinom(yUnique[i], mu = mu, size = 1)
+             
+           })
+    
+    
+    
+  }
+  
+  
+  
+  if(se)
+    
+    rval <- list(rval,out)
+  
+  
+  
+  rval
+  
+}
+
+
+
+
+
+
+
 # 10. PLOT RESPONSES ------------------------------------
 
+# 
+# #--------------------------------------------------
+# # Fit models
+# #--------------------------------------------------
+# 
+# m_pois <- glm(
+#   bycatch ~ Effort + night * kite,
+#   family = poisson,
+#   data = data
+# )
+# 
+# m_nb <- glm.nb(
+#   bycatch ~ Effort + night * kite,
+#   data = data
+# )
+# 
+# m_zip <- zeroinfl(
+#   bycatch ~ Effort + night * kite |
+#     Effort + night + kite,
+#   dist = "poisson",
+#   data = data
+# )
+# 
+# m_zinb <- zeroinfl(
+#   bycatch ~ Effort + night * kite |
+#     Effort + night + kite,
+#   dist = "negbin",
+#   data = data
+# )
 
-#--------------------------------------------------
-# Fit models
-#--------------------------------------------------
-
-m_pois <- glm(
-  bycatch ~ Effort + night * kite,
-  family = poisson,
-  data = data
-)
-
-m_nb <- glm.nb(
-  bycatch ~ Effort + night * kite,
-  data = data
-)
-
-m_zip <- zeroinfl(
-  bycatch ~ Effort + night * kite |
-    Effort + night + kite,
-  dist = "poisson",
-  data = data
-)
-
-m_zinb <- zeroinfl(
-  bycatch ~ Effort + night * kite |
-    Effort + night + kite,
-  dist = "negbin",
-  data = data
-)
-
-m_hurdle <- hurdle(
-  bycatch ~ Effort + night * kite,
-  dist = "negbin",
-  data = data
-)
-
-#--------------------------------------------------
-# Create prediction dataset
-#--------------------------------------------------
+## 10.1.  Create prediction dataset --------------------------------------------
 
 newdat <- expand.grid(
   night = levels(data$night),
@@ -326,15 +492,13 @@ newdat <- expand.grid(
 
 newdat$Effort <- mean(data$Effort, na.rm = TRUE)
 
-#--------------------------------------------------
-# Function to obtain predictions + 95% CI
-#--------------------------------------------------
+
+
+## 10.2.  Function to obtain predictions + 95% CI --------------------------------------------------
 
 get_predictions <- function(model, model_name, newdata) {
   
-  #-------------------------
   # GLM / Negative Binomial
-  #-------------------------
   if (inherits(model, "glm") || inherits(model, "negbin")) {
     
     pred <- predict(
@@ -352,27 +516,24 @@ get_predictions <- function(model, model_name, newdata) {
     
   }
   
-  #-------------------------
   # Zero-inflated / Hurdle
-  #-------------------------
   else if (
     inherits(model, "zeroinfl") ||
     inherits(model, "hurdle")
   ) {
     
-    library(marginaleffects)
-    
-    pred <- predictions(
+    pred <- predict.zeroinfl(
       model,
+      se=T,
       newdata = newdata,
       type = "response"
     )
     
     out <- newdata
     
-    out$fit <- pred$estimate
-    out$lwr <- pred$conf.low
-    out$upr <- pred$conf.high
+    out$fit <- pred[[1]]
+    out$lwr <- pred[[2]]$lower
+    out$upr <- pred[[2]]$upper
     
   } else {
     
@@ -390,92 +551,81 @@ get_predictions <- function(model, model_name, newdata) {
   out
 }
 
-#--------------------------------------------------
-# Collect predictions
-#--------------------------------------------------
+## 10.3. Collect predictions --------------------------------------------------
 
 pred_all <- bind_rows(
   
   get_predictions(m_pois,   "Poisson",      newdat),
   get_predictions(m_nb,     "NegBin",       newdat),
-  get_predictions(m_zip,    "ZIP",          newdat),
-  get_predictions(m_zinb,   "ZINB",         newdat),
-  get_predictions(m_hurdle, "Hurdle NB",    newdat)
+  #get_predictions(m_zip,    "ZIP",          newdat),
+  get_predictions(m_zinb,   "ZINB",         newdat)
+  #get_predictions(m_hurdle, "Hurdle NB",    newdat)
   
 )
 
-# create treatment labels
+## 10.3. create treatment labels ----------------------------------------------
 
 pred_all <- pred_all %>%
   mutate(
-    treatment = paste0(
-      "Night=", night,
-      ", Kite=", kite
-    )
+    treatment = if_else(kite==0,
+                        if_else(night==0,"control","night"),
+                        if_else(night==0,"kite","night_kite"))
   )
 
 # inspect estimates
 pred_all
+pred_all<-pred_all %>%
+  mutate(upr=if_else(is.infinite(upr),0.5,upr))
 
 
 
 
-ggplot(
-  pred_all,
-  aes(
-    x = treatment,
-    y = fit,
-    color = model
-  )
-) +
-  geom_point(
-    position = position_dodge(width = 0.6),
-    size = 3
-  ) +
-  geom_errorbar(
-    aes(ymin = lwr, ymax = upr),
-    width = 0.15,
-    position = position_dodge(width = 0.6)
-  ) +
-  labs(
-    x = "",
-    y = "Predicted mean bycatch",
-    color = "Model"
-  ) +
-  theme_bw() +
-  theme(
-    axis.text.x = element_text(
-      angle = 45,
-      hjust = 1
-    )
-  )
+## 10.4. PLOT ALL JOINED ESTIMATES ----------------------------------------------
+
+## 10.4.1 LOAD bootstrap data 
+boot<-fread("output/boot_estimates.csv") %>%
+  mutate(model=if_else(model=="bca","BCA_boot","simple_boot")) %>%
+  dplyr::select(model, treatment, mean, lcl, ucl)
+
+COMP_PLOT<-pred_all %>%
+  rename(mean=fit, lcl=lwr, ucl=upr) %>%
+  dplyr::select(model, treatment, mean, lcl, ucl) %>%
+  bind_rows(boot) %>%
+  mutate(model=factor(model, levels=c('simple_boot','BCA_boot','NegBin','Poisson','ZINB'))) %>%
 
 
 
 ggplot(
-  pred_all,
   aes(
-    x = treatment,
-    y = fit
+    x = model,
+    y = mean,
+    col=treatment
   )
 ) +
   geom_point(size = 3) +
   geom_errorbar(
-    aes(ymin = lwr, ymax = upr),
+    aes(ymin = lcl, ymax = ucl),
     width = 0.15
   ) +
-  facet_wrap(~ model, scales="free_y") +
+  facet_wrap(~ treatment, scales="fixed") +
   labs(
-    x = "",
-    y = "Predicted mean bycatch"
+    x = "Model type",
+    y = "Predicted mean BPUE"
   ) +
-  theme_bw() +
-  theme(
-    axis.text.x = element_text(
-      angle = 45,
-      hjust = 1
-    )
-  )
+  scale_colour_discrete(guide="none")+
+  theme(panel.background=element_rect(fill="white", colour="black"), 
+        axis.text.x=element_text(size=12, color="black", angle=45, ,hjust = 1), 
+        axis.text.y=element_text(size=16, color="black"), 
+        axis.title=element_text(size=20),
+        axis.title.y=element_text(margin=margin(0,15,0,0)),
+        axis.title.x=element_text(margin=margin(15,0,0,0)), 
+        strip.text=element_text(size=18, color="black"),
+        strip.background=element_rect(fill="white", colour="black"),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        panel.border = element_blank())
+COMP_PLOT
+ggsave("output/BPUE_GLM_assessment.jpg", width=11, height=9)
 
 
 
